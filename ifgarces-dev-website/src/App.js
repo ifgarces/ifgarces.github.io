568fc17e5764a671cc36d5e79a321bbe1a0c7ec0
Ignacio F. Garcés <48460500+ifgarces@users.noreply.github.com>
2024-04-21 20:43:46 -0400

    Added WIP notice to `App` component

--- ifgarces-dev-website/src/App.js
+++ ifgarces-dev-website/src/App.js
@@ -8,10 +8,12 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>WIP</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <code>
+            This is about to be <strong>ifgarces's</strong> portfolio website.
+          </code>
         </p>
       </div>
     );
