18fe2c4bbf40667240ad3514203dc24785180a0e
Ignacio F. Garcés <48460500+ifgarces@users.noreply.github.com>
2024-11-27 20:28:05 -0300

    Added reference website that inspired me! and some other changes I forgot about already

---
+++ reference/parsity.io_files/8389530(1).js
@@ -0,0 +1,41 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+// HubSpot Script Loader. Please do not block this resource. See more: http://hubs.ly/H0702_H0
+
+!function(e,t){if(!document.getElementById(e)){var c=document.createElement("script");c.src="https://web.archive.org/web/20230525060521/https://js.hs-analytics.net/analytics/1684994700000/8389530.js",c.type="text/javascript",c.id=e;var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(c,n)}}("hs-analytics");
+!function(t,e,r){if(!document.getElementById(t)){var n=document.createElement("script");for(var a in n.src="https://web.archive.org/web/20230525060521/https://js.hscollectedforms.net/collectedforms.js",n.type="text/javascript",n.id=t,r)r.hasOwnProperty(a)&&n.setAttribute(a,r[a]);var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("CollectedForms-8389530",0,{"crossorigin":"anonymous","data-leadin-portal-id":8389530,"data-leadin-env":"prod","data-loader":"hs-scriptloader","data-hsjs-portal":8389530,"data-hsjs-env":"prod","data-hsjs-hublet":"na1"});
+var _hsp = window._hsp = window._hsp || [];
+ _hsp.push(['addEnabledFeatureGates', []]);
+!function(t,e,r){if(!document.getElementById(t)){var n=document.createElement("script");for(var a in n.src="https://web.archive.org/web/20230525060521/https://js.hs-banner.com/v2/8389530/banner.js",n.type="text/javascript",n.id=t,r)r.hasOwnProperty(a)&&n.setAttribute(a,r[a]);var i=document.getElementsByTagName("script")[0];i.parentNode.insertBefore(n,i)}}("cookieBanner-8389530",0,{"data-cookieconsent":"ignore","data-hs-ignore":true,"data-loader":"hs-scriptloader","data-hsjs-portal":8389530,"data-hsjs-env":"prod","data-hsjs-hublet":"na1"});
+
+}
+/*
+     FILE ARCHIVED ON 06:05:21 May 25, 2023 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 23:25:29 Nov 27, 2024.
+     JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
+
+     ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
+     SECTION 108(a)(3)).
+*/
+/*
+playback timings (ms):
+  captures_list: 0.425
+  exclusion.robots: 0.016
+  exclusion.robots.policy: 0.007
+  esindex: 0.01
+  cdx.remote: 120.369
+  LoadShardBlock: 267.453 (3)
+  PetaboxLoader3.datanode: 260.591 (4)
+  PetaboxLoader3.resolve: 175.57 (2)
+  load_resource: 206.544
+*/
